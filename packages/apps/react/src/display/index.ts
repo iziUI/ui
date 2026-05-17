8eaadbcd77ec322a291775fc42bb62ed885a19b6
Leonardo Melo <leonardo.melo@nsx.bet>
2026-05-17 00:31:31 -0300

    feat: export components

--- packages/apps/react/src/display/index.ts
+++ packages/apps/react/src/display/index.ts
@@ -1,5 +1,7 @@
+export * from './Avatar';
 export * from './Card';
 export * from './Chip';
+export * from './Divider';
 export * from './Icon';
 export * from './Table';
 export * from './Tooltip';
